7c50d3c2000c73b087099b90d5d1cd1d54badc00
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-09-03 02:07:56 +0500

    Divider done for desktop and mobile

--- layouts/Divider.js
+++ layouts/Divider.js
@@ -3,10 +3,10 @@ import React from "react";
 
 function Divider({ img, link = "/", showArrow = false }) {
   return (
-    <div className="flex items-center justify-center relative space-x-2 my-36">
+    <div className="flex items-center justify-center relative space-x-2 py-16 md:my-20 xl:my-26">
       <div className="h-[1px] w-full bg-purple z-[-1]"></div>
       <Link href={link}>
-        <a className="block min-w-[101px] w-[101px]">
+        <a className="block min-w-[50px] w-[50px] md:min-w-[70px] md:w-[70px] xl:min-w-[101px] xl:w-[101px]">
           <img src={img} className="w-full" alt="" />
         </a>
       </Link>
@@ -15,7 +15,7 @@ function Divider({ img, link = "/", showArrow = false }) {
       {showArrow ? (
         <img
           src="images/side-icon-right.svg"
-          className="absolute top-[50%] right-0 translate-y-[-50%] bg-black"
+          className="absolute top-[50%] right-0 translate-y-[-50%] bg-black w-[4.6rem] sm:w-[6rem] xl:w-[9rem]"
           alt=""
         />
       ) : null}
