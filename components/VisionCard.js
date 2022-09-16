dd6b9bafff70e0f8daa7a0577e21447f843d5aaa
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-09-16 11:45:15 +0500

    set for mobile

--- components/VisionCard.js
+++ components/VisionCard.js
@@ -3,14 +3,14 @@ import BulletPoint from "./BulletPoint";
 
 function VisionCard({ title, img, desc, points }) {
   return (
-    <div className="rounded-3xl bg-[#0d0e12ba] py-8 pt-0 flex flex-col">
+    <div className="rounded-2xl xl:rounded-3xl bg-[#0d0e12ba] py-8 pt-0 flex flex-col">
       <img
         src={img}
         alt=""
-        className="mx-auto mb-8 w-20 h-20 rounded-full -mt-10"
+        className="mx-auto mb-6 sm:mb-8 w-16 xl:w-20 h-16 xl:h-20 rounded-full -mt-10"
       />
 
-      <div className="px-12 h-[20em]">
+      <div className="px-8 xl:px-12 mb-10 lg:mb-0 lg:h-[16em] xl:h-[20em]">
         <h1 className="text-center font-bold text-white fs-28px mb-4">
           {title}
         </h1>
