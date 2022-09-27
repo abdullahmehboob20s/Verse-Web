2475c0a87d10a9142bbc84e561e7bfdb34e112fa
ABDULLAH MEHBOOB <abdullahmehboob2002@gmail.com>
2022-09-27 10:16:16 +0500

    implemented tooltip

--- components/PatentCard.js
+++ components/PatentCard.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Tooltip from "./Tooltip";
 
 function PatentCard({ index, img }) {
   return (
     <div className="space-y-2">
       <p className="fs-24px text-white font-medium text-center">{index}</p>
-      <img src={img} className="h-[120px]" alt="" />
+      <Tooltip text="my name is abdullah mehboob">
+        <img src={img} className="h-[120px] cursor-pointer" alt="" />
+      </Tooltip>
     </div>
   );
 }
